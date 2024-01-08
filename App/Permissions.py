25ab231b1cdc0cb99eb08189631edce62fde0860
xSk09x <olegordei@gmail.com>
2024-01-08 14:16:38 -0600

    CRUD commit

--- App/Permissions.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import permissions
-
-
-# class ReadOnlyPermission(permissions.BasePermission):
-#     def has_permission(self, request, view):
-#         return request.method == 'GET'
