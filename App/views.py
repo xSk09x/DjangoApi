14d6de345c417b9a48ef4b82a5401fbf6ed849eb
xSk09x <olegordei@gmail.com>
2024-01-08 14:12:05 -0600

    CRUD commit

--- App/views.py
+++ App/views.py
@@ -2,6 +2,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
 from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from .models import Plant
@@ -11,6 +12,7 @@ from .serializers import PlantSerializer
 class PlantViewSet(ModelViewSet):
     queryset = Plant.objects.all()
     serializer_class = PlantSerializer
+    permission_classes = [AllowAny]
     parser_classes = (MultiPartParser, FormParser)
     filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
     filter_fields = ['price']
